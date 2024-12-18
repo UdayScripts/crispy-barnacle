570e44b3baf5bbf533ff4d4b855c1fda075ee246
Uday <110768804+UdayScripts@users.noreply.github.com>
2024-12-18 22:24:15 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import os
 import math
 from telebot.types import Message
 from keep_alive import keep_alive
+keep_alive()
 
 # Replace with your bot token and channel username
 BOT_TOKEN = "8142385461:AAFUCFo0ngYrMsGovyquODaFJ5cvd0kgSLQ"
